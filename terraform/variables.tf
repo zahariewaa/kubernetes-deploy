a82aba3a88477a672121395714bec95ee9f1466b
Anastasia Kichukova <akichukova@evolution>
2024-05-30 08:58:47 +0300

    Change region for GCP

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,17 @@
 variable "project_id" {
+  description = "The ID of the project in which resources will be created."
   type        = string
-  description = "The ID of the GCP project."
+  default     = "gcp-project-pract"
 }
 
 variable "region" {
   type        = string
-  description = "The region to deploy resources in."
-  default     = "us-central1"
+  description = "The region in which resources will be created."
+  default     = "europe-west3"
 }
 
 variable "cluster_name" {
   type        = string
   description = "The name of the GKE cluster."
-  default     = "my-gke-cluster"
+  default     = "gke-cluster"
 }
